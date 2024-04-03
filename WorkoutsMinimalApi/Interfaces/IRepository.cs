a3ae9f85119f1ed4a4e6ff63564956e9e2d5e514
baroniunas91 <61449435+baroniunas91@users.noreply.github.com>
2024-04-03 11:38:17 +0300

    Update IRepository.cs

--- WorkoutsMinimalApi/Interfaces/IRepository.cs
+++ WorkoutsMinimalApi/Interfaces/IRepository.cs
@@ -7,6 +7,8 @@ public interface IRepository<TEntity> where TEntity : class, IEntity
     Task<IEnumerable<TEntity>> GetAllAsync();
     Task AddAsync(TEntity entity);
     void Remove(TEntity entity);
+    void Update(TEntity entity);
     Task SaveChangesAsync();
     Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> expression);
+    Task<TEntity> FindSingleAsync(Expression<Func<TEntity, bool>> expression);
 }
